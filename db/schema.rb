13c256b23116df484083bcf4fa1df98495f9adba
Kelly Strickland <imloved@embarqmail.com>
2015-03-13 11:25:42 -0400

    Added user model with name and email.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150312011522) do
+ActiveRecord::Schema.define(version: 20150313152033) do
 
   create_table "items", force: :cascade do |t|
     t.text     "name"
@@ -19,6 +19,13 @@ ActiveRecord::Schema.define(version: 20150312011522) do
     t.date     "ends_on"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "name"
+    t.string   "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
