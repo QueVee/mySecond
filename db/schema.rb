b91332329cf39e1ca34122d9dc18730c3693578f
Kelly Strickland <imloved@embarqmail.com>
2015-03-16 11:08:06 -0400

    Added password_digest, password features and tested sign up functionality.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150313152033) do
+ActiveRecord::Schema.define(version: 20150316140402) do
 
   create_table "items", force: :cascade do |t|
     t.text     "name"
@@ -24,8 +24,9 @@ ActiveRecord::Schema.define(version: 20150313152033) do
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.string   "email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
   end
 
 end
